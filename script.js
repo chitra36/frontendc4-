af62d77bcd633968eaf44620e54b6e959554962b
Chitra bisht <104213057+chitra36@users.noreply.github.com>
2023-04-04 22:28:03 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,39 @@
+/** @format */
+
+let arr = [
+    { id: 1, name: "john", age: "18", marks: 80 },
+    { id: 2, name: "jack", age: "20", marks: 85 },
+    { id: 3, name: "karen", age: "19", marks: 35 },
+  ];
+  
+  function PrintStudentswithMap(Value1) {
+    //Write your code here , just console.log
+    
+    Value1=myMap.get(marks>50);
+    console.log(Value1);
+    
+  }
+  
+  function PrintStudentsbyForEach(num) {
+    //Write your code here , just console.log
+    
+  }
+  function addData(arr1) {
+    //Write your code here, just console.log
+arr1=[{id: 4, name: "susan",age: "20",marks: 45}];
+const add = arr.concat(arr1); 
+console.log(add);
+  }
+  
+  function removeFailedStudent() {
+    //Write your code here, just console.log
+    arr.marks=50;
+  }
+  
+  function concatenateArray() {
+    //Write your code here, just console.log
+    const map1=[{id:1, name:"chitra",age:"22",marks:80},
+               {id:2, name:"johny",age:"27",marks:20},
+               {id:3, name:"carly",age:"12",marks:40}];
+   console.log(map1);
+  }  
